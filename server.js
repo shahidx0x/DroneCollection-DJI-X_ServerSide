658017b942480dad653775fb1b025eb0ce0f9439
shahidx0x <shahidabdulla.aas@gmail.com>
2021-11-13 15:19:43 +0600

    better

--- server.js
+++ server.js
@@ -169,6 +169,14 @@ client.connect((err) => {
         console.log("deleteing user with id", result);
         res.json(result);
       });
+      //DELETE API FOR ADMIN PRODUCTS
+      app.delete("/products/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const result = await haiku3.deleteOne(query);
+        console.log("deleteing user with id", result);
+        res.json(result);
+      });
     } finally {
       // await client.close();
     }
